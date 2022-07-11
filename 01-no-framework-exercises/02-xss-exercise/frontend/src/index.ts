f3a52f26fe8772ffca1fd998ca2ae9c06a60786c
juanpms2 <juanpablo@lemoncode.net>
2022-07-11 14:28:42 +0200

    added cors and conect front to backend

--- 01-no-framework-exercises/02-xss-exercise/frontend/src/index.ts
+++ 01-no-framework-exercises/02-xss-exercise/frontend/src/index.ts
@@ -1,7 +1,7 @@
-document.getElementById("submit").addEventListener("click", function (e) {
-	// <img src='x' onerror='alert("la hemos liao")'>
-	const input = document.getElementById("name") as HTMLInputElement | null;
-	const contenido = input?.value;
+const form = document.getElementById("MyForm");
 
-	document.getElementById("result").innerHTML = contenido;
+form.addEventListener("submit", async (event) => {
+	await fetch("http://localhost:3000")
+		.then((res) => console.log("hola"))
+		.catch((error) => console.log("error"));
 });
