e6bf85ee427c7e993f0697ecc8bd59be2375270d
manudous <you@example.com>
2022-09-07 12:17:04 +0200

    update readme 01 xss exercise no framework

--- 01-no-framework-exercises/01-xss-exercise/src/index.ts
+++ 01-no-framework-exercises/01-xss-exercise/src/index.ts
@@ -1,5 +1,4 @@
 document.getElementById("submit").addEventListener("click", function (e) {
-	// <img src='x' onerror='alert("la hemos liao")'>
 	const input = document.getElementById("name") as HTMLInputElement | null;
 	const contenido = input?.value;
 
